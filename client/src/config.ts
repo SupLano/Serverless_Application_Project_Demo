27d22a9f7d7c4d8f1869e9451ee620436e6b6473
SupLano <myemail@example.com>
2022-11-12 00:41:27 +0100

    Development set, set Production

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
+const apiId = 'uw4p1yl61e';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-wnaj625375r7akzi.us.auth0.com',
+  clientId: 'midMfe16FxIVfBUlqSzLRT5aq1uENw9X',
   callbackUrl: 'http://localhost:3000/callback'
 };
